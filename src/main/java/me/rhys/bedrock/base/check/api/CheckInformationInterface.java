2025945cb03477665939d844d74c7a23ec951c7e
Rhys <codefly44@gmail.com>
2021-04-14 00:45:52 +0100

    Delete CheckInformationInterface.java

--- src/main/java/me/rhys/bedrock/base/check/api/CheckInformationInterface.java
+++
@@ -1,8 +0,0 @@
-package me.rhys.bedrock.base.check.api;
-
-public interface CheckInformationInterface {
-    String checkName();
-    String checkType();
-    String description();
-    boolean enabled();
-}
